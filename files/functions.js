3edce5e4d9f98d08f261001900d147706f6f6a4a
gusejuice <jbsnowden05@icloud.com>
2024-10-14 21:23:58 -0400

    for pm4

---
+++ files/functions.js
@@ -0,0 +1,13 @@
+function change_dark_state() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = document.getElementsByTagName("body")[0].style.backgroundColor == "white" ? "black" : "white";
+    document.getElementsByTagName("body")[0].style.color = document.getElementsByTagName("body")[0].style.color == "black" ? "whitesmoke" : "black";
+}
+
+function on_nav_bar_press(c) {
+    if(c == "study") {
+        document.getElementsByClassName("main-study-group")[0].style.visibility = document.getElementsByClassName("main-study-group")[0].style.visibility == "visible" ? "hidden" : "visible";
+        document.getElementsByClassName("main-study-group-section-problem")[0].style.visibility = document.getElementsByClassName("main-study-group-section-problem")[0].style.visibility == "visible" ? "hidden" : "visible";
+        document.getElementsByClassName("main-study-group-section-affinity")[0].style.visibility = document.getElementsByClassName("main-study-group-section-affinity")[0].style.visibility == "visible" ? "hidden" : "visible";
+        document.getElementsByClassName("main-study-group-section-sketch")[0].style.visibility = document.getElementsByClassName("main-study-group-section-sketch")[0].style.visibility == "visible" ? "hidden" : "visible";
+    }
+}
